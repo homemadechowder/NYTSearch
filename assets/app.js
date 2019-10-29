6a86c82f846d91822fd11caadeb92b776a91a106
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-10-28 19:36:22 -0700

    update js file

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,44 @@
-console.log("hello")
-
 $("#search").on("click", function () {
 
     event.preventDefault();
 
-    console.log("cat")
-    var searchTerm = $("#searchTerm").val().trim()
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=EzFv4pFaLMNlVTTgSTtW5w8pCLGc7YvK";
+    var searchTerm = $("#searchTerm").val().trim();
+    // var count = 5;
     // console.log($("#recordsRetreieve option:selected".text()));
+    var count = $("#recordsRetrieve").val();
+    var startYear = $("#startYear").val().trim();
+    var endYear = $("#endYear").val().trim();
 
-var count = 5; // currently hard coded as 5, but in future is 
+    var queryURL;
+
+    if (startYear === "" && endYear === "") {
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=EzFv4pFaLMNlVTTgSTtW5w8pCLGc7YvK";
+    } else if (startYear !== "" && endYear === "") {
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&begin_date=" +startYear + "&api-key=EzFv4pFaLMNlVTTgSTtW5w8pCLGc7YvK";
+    } else if (startYear === "" && endYear !=="") { 
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&end_date=" + endYear + "&api-key=EzFv4pFaLMNlVTTgSTtW5w8pCLGc7YvK";
+    } else  {
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&end_date=" + endYear + "&begin_date=" + startYear + "&api-key=EzFv4pFaLMNlVTTgSTtW5w8pCLGc7YvK";
+    }
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (results) {
+        console.log(results)
         var articleArray = results.response.docs
+
+        $("#topArticles").empty()
+
+        for (var i = 0; i < count; i++){
+            var headline = articleArray[i].headline.main;
+            var url = articleArray[i].web_url;
+            var abstract = articleArray[i].abstract;
+            var author = articleArray[i].byline.original;
+
+            $("#topArticles").append("<h1>" + headline + "</h1>").append("<hr>").append("<a href=" + url + ">" + url + "</a>").append("<p>" + abstract + "</p>").append("<p>" + author + "</p>");
+            
+        }
 
         // for loop for each element of array
         // count times 
@@ -26,7 +50,5 @@ var count = 5; // currently hard coded as 5, but in future is
         //empty div top articles 
         // $("#topArticles").append()
     })
-
-  
 
 })
